c1cd9c7860e895c112fe8019746d0183d09c5bdc
Sergey Ulin <PYLIN2002@yandex.ru>
2024-03-23 17:10:58 +0300

    Initial commit

---
+++ src/main/java/pro/sky/storeapp/servise/OrderService.java
@@ -0,0 +1,11 @@
+package pro.sky.storeapp.servise;
+
+import java.util.List;
+
+public interface OrderService {
+    List<Integer> addItems(List<Integer> ids);
+
+    List<Integer> addItems(List<Integer> ids);
+
+    List<Integer> getItems();
+}
